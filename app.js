5c6bb9fd6047c615eb639e58185f17589b1d81be
chrisso emma <chrissoemma@gmail.com>
2019-04-13 17:09:29 +0300

    catch all errors format

--- app.js
+++ app.js
@@ -6,15 +6,16 @@ const RoutePosts = require('./routes/api/posts');
 //app.use('/api/v1/posts',RoutePosts);
 app.use((req, res, next) => {
     const error = new Error('Not found');
-    res.status() = 404;
+    error.status = 404;
     next(error);
 });
-
 app.use((error, req, res, next) => {
     res.status(error.status || 500);
     res.json({
         error: {
-            message: error.message
+            code: error.status,
+            message: error.message,
+            stack: error.stack
         }
     });
 });
